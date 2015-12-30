d4e198c88a5fa99fca21cb38681105e7a4f73dc5
Shih-Chiang Chien <schien@mozilla.com>
2015-12-30 18:48:03 +0800

    marketplace v2

---
+++ tv_apps/marketplace/media/locales/en-US.js
@@ -0,0 +1,8 @@
+(function() {
+if (!navigator.l10n) {navigator.l10n = {};}
+navigator.l10n.language = "en-US";
+navigator.l10n.strings = {};
+navigator.l10n.pluralize = function(n) {
+  return 0;
+};
+})();
